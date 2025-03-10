04ed741eb044d78ed08ecc5bb96a4ee13beeb7a5
paul-starr <p.s.starr@ntlworld.com>
2025-03-10 10:52:09 +0000

    add GET /api endpoint for returning all the currently existing APIs on the app

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,13 @@
 const endpointsJson = require("../endpoints.json");
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data/index');
+const request = require('supertest');
+const app = require('../app.js')
 /* Set up your test imports here */
 
-/* Set up your beforeEach & afterAll functions here */
+beforeAll(() => seed(data));
+afterAll(() => db.end());
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -13,3 +19,5 @@ describe("GET /api", () => {
       });
   });
 });
+
+
---
+++ db/MVC/Controllers/api.controllers.js
@@ -0,0 +1,8 @@
+const endpoints = require('../../../endpoints.json')
+
+const getApiEndpoints = (req, res) => {
+    res.status(200).send({endpoints: endpoints})
+}
+
+
+module.exports = getApiEndpoints
